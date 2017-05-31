391807637a3ce8068cf2de55e3c3de2a14adbafb
Lenin Luque <xleninx@gmail.com>
2017-05-31 14:11:48 -0400

    fix engine and add space on view

--- lib/domain_status/engine.rb
+++ lib/domain_status/engine.rb
@@ -1,16 +1,13 @@
 module DomainStatus
   class Engine < ::Rails::Engine
     isolate_namespace DomainStatus
+    require 'slim'
 
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.assets false
       g.helper false
-    end
-
-    Gem.loaded_specs['domain_status'].dependencies.each do |d|
-      require d.name
     end
 
   end
